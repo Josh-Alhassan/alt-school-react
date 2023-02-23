071e162977a9f5354699bc37168e13475753e956
Saint-Josh <41088053+Josh-Alhassan@users.noreply.github.com>
2023-02-23 14:15:41 +0100

    mapping through an array

--- src/App.js
+++ src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import './style.css';
 
 function Car({ data }) {
-  const { brand, color, year } = data;
+  // const { data.brand, data.color, data.year } = data;
   return (
     <div className="car">
-      {year}, {color}, {brand}
+      {data.year}, {data.color}, {data.brand}
     </div>
   );
 }
 
 export default function App() {
+  const cars = [
+    {
+      brand: 'Ford',
+      color: 'Red',
+      year: '2020',
+    },
+    {
+      brand: 'Tesla',
+      color: 'blue',
+      year: '2019',
+    },
+    {
+      brand: 'BMW',
+      color: 'black',
+      year: '2018',
+    },
+  ];
+
+  const listOfCars = cars.map((car) => {
+    return <Car key={Math.floor(Math.random())} data={car} />;
+  });
   return (
     <div>
       <h1>Altschool React Practice!</h1>
-      <Car
-        data={{
-          brand: 'Ford',
-          color: 'Red',
-          year: '2020',
-        }}
-      />
+      <div> {listOfCars} </div>
     </div>
   );
 }
