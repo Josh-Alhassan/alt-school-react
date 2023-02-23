62fba7827295ce815dda3503c55726c00f85686c
Saint-Josh <41088053+Josh-Alhassan@users.noreply.github.com>
2023-02-23 14:04:18 +0100

    objects as props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './style.css';
 
-function Car({ brand, color }) {
+function Car({ data }) {
   return (
     <div className="car">
-      {brand} {color}
+      {data.brand}, {data.color}, {data.year}
     </div>
   );
 }
@@ -13,7 +13,11 @@ export default function App() {
   return (
     <div>
       <h1>Altschool React Practice!</h1>
-      <Car brand="Toyota" color="Silver" />
+      <Car data= {{
+        brand: "Ford",
+        color: "Red",
+        year: "2020"
+      }} />
     </div>
   );
 }
