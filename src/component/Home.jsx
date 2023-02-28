e982c1b1ba6bcba687d386c1dbbea53d0a781bc9
Josh-Alhassan <41088053+Josh-Alhassan@users.noreply.github.com>
2023-02-28 20:10:10 +0100

    Home component

---
+++ src/component/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Routes, Route, Link} from "react-router-dom";
+
+
+function Home() {
+  return (
+    <section>
+      <h1>Home Page </h1>
+    </section>
+  )
+}
