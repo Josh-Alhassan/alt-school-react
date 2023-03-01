e31a7f5ad88fcba801852658363cd592353b6346
Josh-Alhassan <41088053+Josh-Alhassan@users.noreply.github.com>
2023-03-01 14:07:28 +0100

    route about component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Routes, Route, Link} from "react-router-dom"
+import {Routes, Route} from "react-router-dom";
+import Home from "./component/Home";
+import About from "./component/About";
 import './style.css';
 
 export default function App() {
@@ -7,6 +9,10 @@ export default function App() {
   return (
     <section>
       <h1>My App</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </section>
   );
 }
--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Routes, Route, Link} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 
 function Home() {
   return (
     <section>
       <h1>Home Page </h1>
+      <p>Click the Link below to go to the about page</p>
+      <Link to="/About">About Page </Link>
     </section>
   )
 }
+
+export default Home;
